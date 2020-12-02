9a40c571fa88a395fadee3cd4ffd6b1873d1159c
duandazhi <ab601026460@163.com>
2020-12-02 19:09:38 +0800

    使用子账号启动tomcat

---
+++ shell/tomcat.sh
@@ -0,0 +1,8 @@
+使用子账号，启动tomcat
+chown -R tomcat:tomcat /usr/local/tomcat9
+
+su - tomcat  -c '/usr/local/tomcat9/bin/catalina.sh stop'
+su - tomcat  -c '/usr/local/tomcat9/bin/catalina.sh start'
+su - tomcat  -c '/usr/local/tomcat9/bin/catalina.sh run'
+su - tomcat  -c '/usr/local/tomcat9/bin/shutdown.sh'
+su - tomcat  -c 'nohup /usr/local/tomcat9/bin/startup.sh &'
