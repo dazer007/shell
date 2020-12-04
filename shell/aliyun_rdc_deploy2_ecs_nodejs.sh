1af42c80dd7084f10a2b81960c0f8aa75b652069
duandazhi <ab601026460@163.com>
2020-12-04 13:58:15 +0800

    aliyun 发布 nodejs 项目到 ecs 脚本完毕2

--- shell/aliyun_rdc_deploy2_ecs_nodejs.sh
+++ shell/aliyun_rdc_deploy2_ecs_nodejs.sh
@@ -19,10 +19,25 @@ echo '历史文件删除完毕....'
 tar -zxvf /home/nginx/package.tgz -C /home/nginx/application
 echo '解压缩文件完毕..'
 
+echo '-----重新部署nginx start ---------'
 rm -rf /usr/share/nginx/html*
+rm -rf /usr/share/nginx/html2*
 echo '删除nginx /usr/share/nginx/html 完毕'
-
 cp -rf /home/nginx/application/dist /usr/share/nginx/
 echo 'cp dist 目录到 nginx 目录完毕'
+cp -rf /usr/share/nginx/dist /usr/share/nginx/html
+mv /usr/share/nginx/dist /usr/share/nginx/html2
+echo '-----重新部署nginx end success ---------'
+echo ' '
+echo ' '
 
-mv /usr/share/nginx/dist /usr/share/nginx/html
+echo '-----部署到tomcat start---------'
+#rm -rf /usr/local/tomcat9/webapps/ROOT/*.js
+#rm -rf /usr/local/tomcat9/webapps/ROOT/*.css
+#rm -rf /usr/local/tomcat9/webapps/ROOT/*.json
+#rm -rf /usr/local/tomcat9/webapps/ROOT/static
+#rm -rf /usr/local/tomcat9/webapps/ROOT/icons
+#rm -rf /usr/local/tomcat9/webapps/ROOT/workbox-v3.6.3
+#\cp -rf /home/nginx/application/dist/* /usr/local/tomcat9/webapps/ROOT
+echo '-----部署到tomcat 目录 /usr/local/tomcat9/webapps/ROOT/ ---------'
+echo '-----部署到tomcat end success ---------'
