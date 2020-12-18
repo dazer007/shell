de9832c9740971ee0f52a204d524f6ea58d0ade7
duandazhi <ab601026460@163.com>
2020-12-18 17:12:29 +0800

    解决 tomcat 访问 session 问题

--- shell/tomcat.sh
+++ shell/tomcat.sh
@@ -10,4 +10,4 @@ su - tomcat  -c '/usr/local/tomcat9/bin/catalina.sh stop'
 su - tomcat  -c '/usr/local/tomcat9/bin/catalina.sh start'
 su - tomcat  -c '/usr/local/tomcat9/bin/catalina.sh run'
 su - tomcat  -c '/usr/local/tomcat9/bin/shutdown.sh'
-su - tomcat  -c 'nohup /usr/local/tomcat9/bin/startup.sh &'
+su - tomcat  -c 'nohup /usr/local/tomcat9/bin/startup.sh &'  #打印日志到
