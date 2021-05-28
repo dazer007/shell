06616db47c684530a582e8832447580f294f6be8
duandazhi <ab601026460@163.com>
2021-05-28 13:49:01 +0800

    杀死vpn 重新启动

--- windows/公正财富使用/kill_vpn_restart_Vone.bat
+++ windows/公正财富使用/kill_vpn_restart_Vone.bat
@@ -1,6 +1,7 @@
 ﻿echo 'kaishi setting'
 set APPLICATION_NAME=tianrongxin_SV_VPN
-@REM 第一步，杀死应用程序...; /f：强制，/im：指定进程名称;  
+@REM 第一步，杀死应用程序...; /f：强制，/im：指定进程名称;
+@REM taskkill /fi "imagename eq nginx.EXE" /f
 taskkill /f /im SV_Client.exe 
 echo '%APPLICATION_NAME%  have kill succes....'
 
@@ -16,3 +17,6 @@ echo '%APPLICATION_NAME% start success '
 pause
 exit
 exit
+
+
+
